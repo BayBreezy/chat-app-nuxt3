596125a57d3a002ddbb4ca6a769fc3b41a41fe1e
BayBreezy <behon.baker@yahoo.com>
2022-09-28 10:18:54 -0500

    ✨ Add chat list body ui

--- client/pages/account/index.vue
+++ client/pages/account/index.vue
@@ -34,7 +34,7 @@
 		</header>
 		<main class="h-[calc(100vh-76px)] w-full flex">
 			<div class="lg:w-[350px] shrink-0 h-full border-r">
-				<SimpleBar :options="{ autoHide: false }" class="h-full w-full overflow-y-auto">
+				<SimpleBar :options="{ autoHide: true }" class="h-full w-full overflow-y-auto">
 					<div>
 						<!-- Search input -->
 						<div class="sticky top-0 bg-white p-3 z-10">
@@ -82,14 +82,57 @@
 			</div>
 			<div class="grow h-full flex flex-col">
 				<!-- Chat header -->
-				<div class="h-16 bg-fuchsia-200 shrink-0"></div>
-				<div class="grow h-full overflow-y-auto">
-					<ClientOnly>
-						<EmojiPicker :native="true" @select="onSelectEmoji" />
-					</ClientOnly>
+				<div class="h-16 shadow shrink-0 flex items-center px-4 justify-between">
+					<button class="inline-flex items-center space-x-3 text-left group">
+						<span
+							class="p-3 rounded-full bg-gray-100 inline-flex group-hover:bg-gray-200 transition-all"
+						>
+							<Icon size="22" name="ph:user" />
+						</span>
+						<div class="grow">
+							<h2 class="font-semibold text-sm" v-if="userHasProfile">
+								{{ user.profile.firstName }} {{ user.profile.firstName }}
+							</h2>
+							<h2 class="font-semibold" v-else>@{{ user.username }}</h2>
+						</div>
+					</button>
+					<div class="flex items-center">
+						<button>
+							<Icon name="heroicons:ellipsis-vertical-solid" size="24" />
+						</button>
+					</div>
 				</div>
+				<!-- List of messages -->
+				<SimpleBar class="grow h-full overflow-y-auto">
+					<div class="p-5 space-y-2">
+						<template v-for="c in 50" :key="`chatLoaded-${c}`">
+							<div class="p-2 rounded-xl max-w-[60%] shadow bg-white">
+								<p class="text-sm">
+									Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, doloribus fugit
+									quas dolores provident minima aspernatur. Unde assumenda sequi repellendus
+									quisquam placeat optio consequuntur eius distinctio eos, pariatur cum natus?
+								</p>
+								<span class="text-xs">12:14PM</span>
+							</div>
+						</template>
+					</div>
+				</SimpleBar>
 				<!-- chat input -->
-				<div class="h-16 border-t bg-orange-200 shrink-0"></div>
+				<div class="h-16 border-t bg-orange-200 shrink-0">
+					<Popover class="relative" v-slot="{ open }">
+						<PopoverButton>
+							<Icon v-if="!open" name="heroicons:face-smile" size="24" />
+							<Icon v-else name="ph:keyboard" size="24" />
+						</PopoverButton>
+						<transition name="fade">
+							<PopoverPanel class="absolute z-10 bg-white -top-[330px]">
+								<ClientOnly>
+									<EmojiPicker :native="true" @select="onSelectEmoji" />
+								</ClientOnly>
+							</PopoverPanel>
+						</transition>
+					</Popover>
+				</div>
 			</div>
 		</main>
 	</div>
@@ -97,6 +140,7 @@
 
 <script setup>
 	import { SimpleBar } from "simplebar-vue3";
+	import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 	const user = useStrapiUser();
 	const userHasProfile = computed(() => {
 		return !!user.value.profile;
